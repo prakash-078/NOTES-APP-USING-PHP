9dbeed570d422715d11e372fe3a92826d55d56e3
Prakash Kuamr Mallik <mallikprakashkuamr56@gmail.com>
2026-03-18 12:34:44 +0530

    Added Dashboard

---
+++ dashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Dashboard</h2>
+      <div class="link">
+           <a href="add_note.php" class="btn">Add Note</a>
+       </div>
+       <div class="link">
+           <a href="index.php" class="btn">View Notes</a>
+       </div>
+       
+</div>
+</body>
+</html>
